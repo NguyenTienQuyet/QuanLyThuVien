931c01b7da7dd8a9fac3a45361b805fc6728602e
qdung <phanquangdng@gmail.com>
2019-03-19 16:50:00 +0700

    Insert. Create Service family subclasses

---
+++ app/Services/Eloquent/EloquentBookHistoryService.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 3/11/2019
+ * Time: 10:55 PM
+ */
+
+namespace App\Services\Eloquent;
+
+
+use App\Repositories\BookHistoryRepository;
+use App\Services\BookHistoryService;
+
+class EloquentBookHistoryService extends EloquentService implements BookHistoryService
+{
+    public function __construct(BookHistoryRepository $repository)
+    {
+        parent::__construct($repository);
+    }
+}
---
+++ app/Services/Eloquent/EloquentImageService.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 3/11/2019
+ * Time: 11:00 PM
+ */
+
+namespace App\Services\Eloquent;
+
+
+use App\Repositories\ImageRepository;
+use App\Services\ImageService;
+
+class EloquentImageService extends EloquentService implements ImageService
+{
+    public function __construct(ImageRepository $repository)
+    {
+        parent::__construct($repository);
+    }
+}
