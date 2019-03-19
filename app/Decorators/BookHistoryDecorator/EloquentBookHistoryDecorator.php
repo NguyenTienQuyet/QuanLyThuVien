040fdcbcbe37ea86300fcb9e31904a76923c357b
qdung <phanquangdng@gmail.com>
2019-03-19 17:07:01 +0700

    Create Book history decorator

---
+++ app/Decorators/BookHistoryDecorator/EloquentBookHistoryDecorator.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 3/16/2019
+ * Time: 2:30 PM
+ */
+
+namespace App\Decorators\BookHistoryDecorator;
+
+
+use App\Decorators\EloquentDecorator;
+use App\Services\BookHistoryService;
+
+class EloquentBookHistoryDecorator extends EloquentDecorator implements BookHistoryService
+{
+    public function __construct(BookHistoryService $service)
+    {
+        parent::__construct($service);
+    }
+}
