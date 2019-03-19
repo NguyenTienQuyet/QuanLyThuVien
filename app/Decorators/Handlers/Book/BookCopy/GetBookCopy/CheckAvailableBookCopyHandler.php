3c92db95219acfd6217f9695a782ef13b79b6be9
qdung <phanquangdng@gmail.com>
2019-03-19 16:59:46 +0700

    Insert. Create Get BookCopy Handlers

---
+++ app/Decorators/Handlers/Book/BookCopy/GetBookCopy/CheckAvailableBookCopyHandler.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 3/14/2019
+ * Time: 11:05 AM
+ */
+
+namespace App\Decorators\Handlers\Book\BookCopy\GetBookCopy;
+
+
+use App\Decorators\Handlers\Book\BookCopy\BookCopyHandler;
+use App\Decorators\Handlers\HandlerResponseCreators\HandlerResponse;
+
+class CheckAvailableBookCopyHandler extends BookCopyHandler
+{
+    private static $ERROR_MESSAGE = 'Invalid input quantity';
+    public function handle(array &$attributes): HandlerResponse
+    {
+        $bookCopyService = $this->createHandlerService();
+
+        //gather get information
+        $bookId = $attributes['bookId'];
+        $desiredQuantity = $attributes['quantity'];
+        //set up searching information
+        $pairs = [
+            [
+                'needle' => 'book_id',
+                'value' => $bookId
+            ],
+            [
+                'needle' => 'state',
+                'value' => true
+            ],
+        ];
+
+        $availableQuantity = $bookCopyService->count($pairs);
+
+        if ($desiredQuantity > $availableQuantity) {
+            return $this->createHandlerResponse(self::$ERROR_MESSAGE, false);
+        }
+
+        return parent::handle($attributes);
+    }
+}
