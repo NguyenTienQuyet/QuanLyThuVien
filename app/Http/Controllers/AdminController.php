839946e4070d13be032eef446f1c2208a4c23423
Phi Van Tuan <20154139@student.hust.edu.vn>
2019-04-06 23:27:03 +0700

    merge template admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,7 +7,41 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     //
+    
     public function getHome(){
     	return view('admin.home');
     }
+
+    public function getListRole(){
+    	return view('admin.listRole');
+    }
+
+    public function getListUser(){
+    	return view('admin.listUser');
+    }
+
+    public function getListBookQuantity(){
+    	return view('admin.listBookQuantity');
+    }
+
+    public function getListAuthorBook(){
+    	return view('admin.listAuthorBook');
+    }
+
+    public function getListBookGenre(){
+    	return view('admin.listBookGenre');
+    }
+
+    public function getListPublisher(){
+    	return view('admin.listPublisher');
+    }
+
+    public function getListAuthor(){
+    	return view('admin.listAuthor');
+    }
+
+    public function getListGenre(){
+    	return view('admin.listGenre');
+    }
+
 }
