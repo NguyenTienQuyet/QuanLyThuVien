3739805664547aa19979614baf827eb636d26718
Phi Van Tuan <20154139@student.hust.edu.vn>
2019-04-11 23:28:28 +0700

    register controller

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,9 +4,25 @@
 namespace App\Http\Controllers;
 
 
+use App\Models\User;
+
 class RegisterController extends Controller
 {
     public function getRegister(){
         return view('register.register');
+    }
+    public function register(Request $request){
+        $this->validate($request,[
+            'name'=>'required',
+            'email'=>'required',
+            'password'=>'required'
+        ]);
+        $user=new User();
+        $user->name = $request->get('name');
+        $user->email = $request->get('email');
+        $user->password = Hash::make($request->get('password'));
+        $user->role_id = 1;
+        $user->save();
+        return view('admin.master');
     }
 }
