9c969be8b49e715288b00a855a6108a33fabbfb2
Phi Van Tuan <20154139@student.hust.edu.vn>
2019-04-16 19:42:49 +0700

    exception register

--- public/js/Register.js
+++ public/js/Register.js
@@ -3,28 +3,37 @@ jQuery(function ($) {
         var name = $('#name_user').val();
         var email = $('#email_user').val();
         var password = $('#password_user').val();
-        $.ajax({
+        var retype_pass = $('#re_password_user').val();
+        if (name == "" || email == "" || password == "" || retype_pass == "") {
+            alert("Nhap day du thong tin")
+        } else if (password != retype_pass) {
+            alert("Mat khau xac nhan khong dung")
+        } else if (password.length<6) {
+            alert("Mat khau phai it nhat 6 ki tu")
+        }else {
+            $.ajax({
 
-            url: "http://127.0.0.1:8000/api/v1/users/register",
-            type: 'post',
-            dataType: "json",
-            data: {
+                url: "http://127.0.0.1:8000/api/v1/users/register",
+                type: 'post',
+                dataType: "json",
+                data: {
 
-                name: name,
-                email: email,
-                password: password,
-                c_password:password
+                    name: name,
+                    email: email,
+                    password: password,
+                    c_password: password
 
-            },
-            success: function () {
-                window.location.href="http://127.0.0.1:8000/homeAdmin"
-                alert("success!");
-            },
-            error: function (err) {
-                console.log(err);
-                alert(err.toString());
-            }
-        });
+                },
+                success: function () {
+                    window.location.href = "http://127.0.0.1:8000/homeAdmin"
+                    alert("success!");
+                },
+                error: function (err) {
+                    console.log(err);
+                    alert(err);
+                }
+            });
+        }
 
 
     })
