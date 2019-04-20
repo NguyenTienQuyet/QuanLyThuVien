f2eb38dad591c0fd1109559f24fa7a123c3e8687
Phi Van Tuan <20154139@student.hust.edu.vn>
2019-04-21 00:12:37 +0700

    fix position button add book

--- resources/views/admin/listBook.blade.php
+++ resources/views/admin/listBook.blade.php
@@ -237,21 +237,23 @@
                     </div>
 
                             </div>
-                        </div>
-                    </div>
-
-
-                </div>
-
-                <br/>
                 <div class="modal-footer">
-                  <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
                     <button class="btn btn-info" type="submit" id="add-book">
 
                         <i class="ace-icon fa fa-check bigger-110"></i>
                         Add
                     </button>
                 </div>
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+                <br/>
+
             </div>
 
         <!-- </form> -->
